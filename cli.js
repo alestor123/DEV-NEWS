4001ed71d3d598273fc5a41fcc2c51991767ae15
alestor123 <alestoraldous@gmail.com>
2021-01-23 14:30:15 +0530

    added main cli codde

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var news = require('./App'),
+chalk = require('chalk');
+(async () => {
+var data = await news();
+data.news.map((item,index) => console.log(`${data.news.length-index} : ${chalk.bold.yellow(item.title)} \n ${chalk.cyanBright(item.url)}`))
+})();
