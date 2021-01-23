161dbc7196f7140733abbabf9bd71bfdfd9f5ac5
alestor123 <alestoraldous@gmail.com>
2021-01-23 14:16:28 +0530

    added main code

--- App.js
+++ App.js
@@ -1,2 +1,11 @@
 var axios = require('axios'),
 news = [];
+module.exports = async page => {
+var page = page || 1,
+data = await axios.get(`https://api.hnpwa.com/v0/news/${page}.json`)
+data.data.map(newss => news.push({title:newss.title,url:newss.url,time:newss.time_ago,username:newss.user}))
+return {
+news:news,
+no:news.length
+}
+}
