3525b5daf1dd09d706dab822df0a452a84110aa9
alestor123 <alestoraldous@gmail.com>
2021-01-23 14:31:00 +0530

    added bin

--- cli.js
+++ cli.js
@@ -2,6 +2,6 @@
 var news = require('./App'),
 chalk = require('chalk');
 (async () => {
-var data = await news();
+var data = await news(process.argv[2]);
 data.news.map((item,index) => console.log(`${data.news.length-index} : ${chalk.bold.yellow(item.title)} \n ${chalk.cyanBright(item.url)}`))
 })();
